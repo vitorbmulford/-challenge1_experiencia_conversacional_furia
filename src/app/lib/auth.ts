55b865e9de1b2a610dd9efe2b8d025ce96a52d7c
Vitor Bebiano Mulford <vitorbmulford@gmail.com>
2025-04-25 16:58:44 -0300

    arrumando auth para vercel

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -1,13 +1,19 @@
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID, 
-      clientSecret: process.env.AUTH_GOOGLE_SECRET, 
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+      authorization: {
+        params: {
+          redirect_uri:
+            "https://furirachatia.vercel.app/api/auth/callback/google",
+        },
+      },
     }),
   ],
-  secret: process.env.AUTH_SECRET, 
-  trustHost: true, 
+  secret: process.env.AUTH_SECRET,
+  trustHost: true,
 });
